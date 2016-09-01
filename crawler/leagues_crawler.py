edf47a3bd1067ce5405d0857a086f2513f5a8918
Jiang Sheng <gisonrg@gmail.com>
2016-09-01 17:17:18 +0800

    Implement leagues and teams crawler

--- crawler/leagues_crawler.py
+++ crawler/leagues_crawler.py
@@ -1,44 +1,92 @@
 import json
 import urllib
-from utils import download, readJson, mkdir, getConfig
+from utils import download, readJson, mkdir, getConfig, buildSteamApiUrl
 from progress.bar import Bar
+import vdf
 
 
-def fetchIconFromCdn(leagueId, resourceUrl, savePath, type):
-    cdnUrl = "http://cdn.dota2.com/apps/570/" + resourceUrl
-    types = {
-        0: "ticket",
-        1: "ticket_large",
-        2: "ingame",
-    }
-    download(cdnUrl, savePath + leagueId + "_" + types[type] + ".png")
+class LeagueCrawler(object):
+    def __init__(self, config):
+        self.config = config
+        self.savePath = config['savepath'] + '/leagues/'
+        self.imageUrl = buildSteamApiUrl('IEconDOTA2_570', 'GetItemIconPath', config['apikey'])
+        self.schemaUrl = buildSteamApiUrl('IEconItems_570', 'GetSchemaURL', config['apikey'])
+        self.cdnUrl = "http://cdn.dota2.com/apps/570/"
 
+    def crawl(self):
+        self.downloadSchema()
+        items = self.loadAndParseVdf()
+        leagues = self.getAllLeagueResource(items)
+        bar = Bar('Crawling Leagues', max=len(leagues))
+        for key, value in leagues.iteritems():
+            # First find out the valid url to use
+            resourceId = value[0]
+            if len(value) > 1:
+                resourceId = self.getValidResourceId(value)
 
-def buildSteamApiUrl(interface, method, apiKey):
-    return "https://api.steampowered.com/{0}/{1}/v1/?key={2}&format=json".format(interface, method, apiKey)
-
-
-def crawl(items, savePath, apiKey):
-    steamUrl = buildSteamApiUrl("IEconDOTA2_570", "GetItemIconPath", apiKey)
-    bar = Bar('Processing', max=len(items))
-    for key, value in items.iteritems():
-        image = value['ticket'].split("/")[-1]
-        for type in range(0, 3):
-            response = urllib.urlopen(steamUrl + "&iconname={0}&icontype={1}".format(image, type))
-            data = json.loads(response.read())
-            if not 'error' in data['result']:
-                imagePath = data['result']['path']
-                fetchIconFromCdn(key, imagePath, savePath, type)
+            if resourceId is None:
                 bar.next()
-    bar.finish()
+                continue
+
+            for type in range(0, 3):
+                response = urllib.urlopen("{0}&iconname={1}&icontype={2}".format(self.imageUrl, resourceId, type))
+                data = json.loads(response.read())
+                if not 'error' in data['result']:
+                    imagePath = data['result']['path']
+                    self.fetchIconFromCdn(key, imagePath, type)
+            bar.next()
+        bar.finish()
+
+    def getValidResourceId(self, list):
+        for id in list:
+            response = urllib.urlopen("{0}&iconname={1}".format(self.imageUrl, id))
+            data = json.loads(response.read())
+            if 'error' in data['result']:
+                continue
+            if 'path' in data['result']:
+                return id
+        return None
+
+    def getAllLeagueResource(self, items):
+        # leagues : {
+        #   league_id : [possible_resource_id]
+        # }
+        leagues = {}
+        for key, value in items.iteritems():
+            if 'prefab' in value and value['prefab'] == 'league':
+                resourcePath = value['image_inventory']
+                if 'tool' in value and 'usage' in value['tool'] and 'league_id' in value['tool']['usage']:
+                    leagueId = value['tool']['usage']['league_id']
+                    resourceId = resourcePath.split("/")[-1]
+                    if leagueId not in leagues:
+                        leagues[leagueId] = [resourceId]
+                    else:
+                        leagues[leagueId].append(resourceId)
+        return leagues
+
+    def loadAndParseVdf(self):
+        d = vdf.load(open('resources/items_game.txt'))
+        items = d['items_game']['items']
+        return items
+
+    def downloadSchema(self):
+        response = urllib.urlopen(self.schemaUrl)
+        res = json.loads(response.read())
+        imageUrl = res['result']['items_game_url']
+        urllib.urlretrieve(imageUrl, 'resources/items_game.txt')
+
+    def fetchIconFromCdn(self, leagueId, resourceUrl, type):
+        cdnUrl = self.cdnUrl + resourceUrl
+        types = {
+            0: "ticket",
+            1: "ticket_large",
+            2: "ingame",
+        }
+        urllib.urlretrieve(cdnUrl, "{0}{1}_{2}.png".format(self.savePath, leagueId, types[type]))
 
 
 if __name__ == '__main__':
     config = getConfig()
-    apiKey =  config["api"]["key"]
-    jsonSrouce = "https://raw.githubusercontent.com/dotabuff/d2vpkr/master/dota/scripts/items/leagues.json"
-    jsonPath = "model/leagues.json"
-    savePath = "{0}/leagues/".format(config["output"]["path"])
-    download(jsonSrouce, jsonPath)
-    mkdir(savePath)
-    crawl(readJson(jsonPath), savePath, apiKey)
+    mkdir(config['savepath'] + '/leagues')
+    crawler = LeagueCrawler(config)
+    crawler.crawl()
---
+++ crawler/teams_crawler.py
@@ -0,0 +1,80 @@
+from utils import buildSteamApiUrl, getConfig, mkdir
+from progress.bar import Bar
+import psycopg2
+import urllib
+import json
+
+
+class TeamsCrawler(object):
+    def __init__(self):
+        self.config = getConfig()
+        self.savePath = config['savepath'] + '/teams/'
+        self.ugcApiUrl = buildSteamApiUrl('ISteamRemoteStorage', 'GetUGCFileDetails', config['apikey'])
+        self.teamsApiUrl = buildSteamApiUrl('IDOTA2Match_570', 'GetTeamInfoByTeamID', config['apikey'])
+
+    def crawl(self):
+        teams = self.getTeamFromDb()
+        bar = Bar('Crawling', max=len(teams))
+        for team in teams:
+            teamId = team[0]
+            logoId = team[1]
+            try:
+                self.loadAndSaveImageToPath(self.getImageUrlByUgcId(logoId),
+                                            self.savePath + '{0}_logo.png'.format(teamId))
+                if team[2] != 0:
+                    sponsorLogoId = team[2]
+                    self.loadAndSaveImageToPath(self.getImageUrlByUgcId(sponsorLogoId),
+                                                self.savePath + '{0}_sponsor.png'.format(teamId))
+            except(RuntimeError, TypeError, NameError, ValueError):
+                print "Failed to process team {0}\n".format(teamId)
+            bar.next()
+        bar.finish()
+
+    def crawlByTeamId(self, teamId):
+        res = self.getTeamInfoById(teamId)
+        if res['result']['status'] != 1 or len(res['result']['teams']) == 0:
+            return
+        team = res['result']['teams'][0]
+        logoId = team['logo']
+        sponsorId = team['logo_sponsor']
+        self.loadAndSaveImageToPath(self.getImageUrlByUgcId(logoId), self.savePath + '{0}_logo.png'.format(teamId))
+        if sponsorId != "":
+            self.loadAndSaveImageToPath(self.getImageUrlByUgcId(sponsorId),
+                                        self.savePath + '{0}_sponsor.png'.format(teamId))
+
+    def getTeamInfoById(self, teamId):
+        url = self.teamsApiUrl + '&start_at_team_id={0}&teams_requested=1'.format(teamId)
+        response = urllib.urlopen(url)
+        res = json.loads(response.read())
+        return res
+
+    def getTeamFromDb(self):
+        conn = psycopg2.connect("dbname={0} user={1} password={2}".format(config['db']['name'], config['db']['user'],
+                                                                          config['db']['password']))
+        cur = conn.cursor()
+        cur.execute("SELECT id, logo_id, logo_sponsor_id FROM dota2_teams WHERE logo_id <> 0")
+        data = cur.fetchall()
+        cur.close()
+        conn.close()
+        return data
+
+    def getImageUrlByUgcId(self, id):
+        url = self.ugcApiUrl + '&ugcid={0}&appid=570'.format(id)
+        response = urllib.urlopen(url)
+        res = json.loads(response.read())
+        if not 'data' in res:
+            return
+        imageUrl = res['data']['url']
+        return imageUrl
+
+    def loadAndSaveImageToPath(self, imageUrl, filepath):
+        if not imageUrl is None:
+            urllib.urlretrieve(imageUrl, filepath)
+
+
+if __name__ == '__main__':
+    config = getConfig()
+    mkdir(config['savepath'] + '/teams') # create the output folder if not exists
+    crawler = TeamsCrawler()
+    crawler.crawl()
+    # crawler.crawlByTeamId(15)
