3d5baaef9f2a15bfec6bfcc358f4b872026e8560
Jiang Sheng <gisonrg@gmail.com>
2016-09-16 16:54:12 +0800

    Add abilities crawler.

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,6 @@
 FROM kyma/docker-nginx
 RUN apt-get update -y
-RUN apt-get install -yqq cron
-RUN apt-get install -yqq libpq-dev python-dev python-pip
-RUN apt-get install -yqq libyaml-dev
-RUN apt-get install -yqq rsyslog
+RUN apt-get install -yqq cron libpq-dev python-dev python-pip libyaml-dev rsyslog
 
 # Set up cron
 ADD crontab /etc/cron.d/hello-cron
@@ -22,9 +19,6 @@ RUN mkdir /usr/local/crawler
 COPY crawler /usr/local/crawler/
 RUN mkdir /usr/local/images/
 RUN mkdir /var/www
-RUN mkdir /var/www/teams
-RUN mkdir /var/www/leagues
-RUN echo "Welcome" > /var/www/index.html
 RUN chmod +x /usr/local/crawler/run_crawler.sh
 RUN pip install --requirement /usr/local/crawler/requirement.txt
 RUN printenv | sed 's/^\(.*\)$/export \1/g' > /root/project_env.sh
---
+++ crawler/abilities_crawler.py
@@ -0,0 +1,31 @@
+from utils import download, readYamlFile, mkdir, getConfig
+from progress.bar import Bar
+
+
+class AbilitiesCrawler(object):
+    def __init__(self):
+        self.config = getConfig()
+        self.savePath = config['savepath'] + '/abilities/'
+        self.constantPath = "dota2-constant/yml/abilities.yml"
+
+    def crawl(self):
+        endpoint = "http://cdn.dota2.com/apps/dota2/images/abilities/"
+        items = readYamlFile(self.constantPath)
+        bar = Bar('Crawling abilities', max=len(items))
+        for key, value in items.iteritems():
+            smname = "{0}_sm.png".format(value)
+            mdname = "{0}_md.png".format(value)
+            lgname = "{0}_lg.png".format(value)
+            download(endpoint + lgname, self.savePath + lgname)
+            download(endpoint + smname, self.savePath + smname)
+            download(endpoint + mdname, self.savePath + mdname)
+            bar.next()
+        bar.finish()
+
+
+if __name__ == '__main__':
+    config = getConfig()
+    savePath = "{0}/abilities/".format(config["savepath"])
+    mkdir(savePath)
+    crawler = AbilitiesCrawler()
+    crawler.crawl()
