c099aa4eaa4aba163e5980c1bb7e1a241f28d294
Jiang Sheng <gisonrg@gmail.com>
2016-10-22 13:23:39 +0800

    Improve the docker file

---
+++ setup.sh
@@ -0,0 +1,20 @@
+# Set up cron
+ADD crontab /etc/cron.d/hello-cron
+
+# Give execution rights on the cron job
+RUN chmod 0644 /etc/cron.d/hello-cron
+
+# Create the log file to be able to run tail
+RUN touch /var/log/cron.log
+
+ENV SAVE_PATH=../images
+ENV VOLUMN_PATH=/var/www
+
+# Copy the crawler to container
+RUN mkdir /usr/local/crawler
+COPY crawler /usr/local/crawler/
+RUN mkdir /usr/local/images/
+RUN mkdir /var/www
+RUN chmod +x /usr/local/crawler/run_crawler.sh
+RUN pip install --requirement /usr/local/crawler/requirement.txt
+RUN printenv | sed 's/^\(.*\)$/export \1/g' > /root/project_env.sh
