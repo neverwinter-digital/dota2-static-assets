bf5f040bd434ed617c996e6bc43781e6a37f0280
Jiang Sheng <gisonrg@gmail.com>
2016-10-22 14:06:39 +0800

    Update dockerfile

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,13 @@ FROM kyma/docker-nginx
 RUN apt-get update -y
 RUN apt-get install -yqq cron libpq-dev python-dev python-pip libyaml-dev rsyslog
 
-RUN ./setup.sh
+ADD setup.sh /root/setup.sh
+# Set up cron
+ADD crontab /etc/cron.d/hello-cron
+# Copy the crawler to container
+RUN mkdir /usr/local/crawler
+COPY crawler /usr/local/crawler/
+
+RUN /root/setup.sh
 
 CMD rsyslogd && cron && nginx
--- setup.sh
+++ setup.sh
@@ -1,26 +1,20 @@
-# Set up cron
-ADD crontab /etc/cron.d/hello-cron
-
 # Give execution rights on the cron job
-RUN chmod 0644 /etc/cron.d/hello-cron
+chmod 0644 /etc/cron.d/hello-cron
 
 # Create the log file to be able to run tail
-RUN touch /var/log/cron.log
+touch /var/log/cron.log
 
-ENV STEAM_API_KEY=A72DE7D7BE9870C8DA671D67941CCAA7
-ENV SAVE_PATH=../images
-ENV VOLUMN_PATH=/var/www
-ENV DB_URL=$POSTGRES_URL
-ENV DB_NAME=$POSTGRES_DATABASE
-ENV DB_PORT=$POSTGRES_PORT
-ENV DB_USER=$POSTGRES_USERNAME
-ENV DB_PASSWORD=$POSTGRES_PASSWORD
+export STEAM_API_KEY=A72DE7D7BE9870C8DA671D67941CCAA7
+export SAVE_PATH=../images
+export VOLUMN_PATH=/var/www
+export DB_URL=$POSTGRES_URL
+export DB_NAME=$POSTGRES_DATABASE
+export DB_PORT=$POSTGRES_PORT
+export DB_USER=$POSTGRES_USERNAME
+export DB_PASSWORD=$POSTGRES_PASSWORD
 
-# Copy the crawler to container
-RUN mkdir /usr/local/crawler
-COPY crawler /usr/local/crawler/
-RUN mkdir /usr/local/images/
-RUN mkdir /var/www
-RUN chmod +x /usr/local/crawler/run_crawler.sh
-RUN pip install --requirement /usr/local/crawler/requirement.txt
-RUN printenv | sed 's/^\(.*\)$/export \1/g' > /root/project_env.sh
+mkdir /usr/local/images/
+mkdir /var/www
+chmod +x /usr/local/crawler/run_crawler.sh
+pip install --requirement /usr/local/crawler/requirement.txt
+printenv | sed 's/^\(.*\)$/export \1/g' > /root/project_env.sh
